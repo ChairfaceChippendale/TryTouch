7c3597a94834a399187462249d93e00197414894
Alexander Taran <olexander.taran@yalantis.com>
2020-08-19 14:57:48 +0300

    update dependencies

--- app/src/main/java/com/ujujzk/trytouch/MainActivity.kt
+++ app/src/main/java/com/ujujzk/trytouch/MainActivity.kt
@@ -5,11 +5,11 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Rect
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
